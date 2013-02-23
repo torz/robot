4be460a9a4f9e1339e79fa3f8df480a3297ab523
rszabo <agent047@gmx.com>
2013-02-23 19:38:58 +0000

    gpio direction and some i2c pwv

---
+++ gpio_stuff.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+
+def forward():
+	GPIO.output(24, False)
+	GPIO.output(25, True)
+
+def back():
+	GPIO.output(24, True)
+	GPIO.output(25, False)
+
+def stop():
+	GPIO.output(24, False)
+	GPIO.output(25, False)
+
