13d07469f86e25935ab4bf4fdd2d2edbc43d9b34
rszabo <agent047@gmx.com>
2013-02-24 15:21:57 +0000

    fixed white spaces 2

--- adafruit/Adafruit_PWM_Servo_Driver.py
+++ adafruit/Adafruit_PWM_Servo_Driver.py
@@ -12,20 +12,20 @@ class PWM :
 	i2c = None
 
 	# Registers/etc.
-	__SUBADR1						= 0x02
-	__SUBADR2						= 0x03
-	__SUBADR3						= 0x04
-	__MODE1							= 0x00
-	__MODE2							= 0x01
-	__PRESCALE					 = 0xFE
-	__LED0_ON_L					= 0x06
-	__LED0_ON_H					= 0x07
-	__LED0_OFF_L				 = 0x08
-	__LED0_OFF_H				 = 0x09
-	__ALLLED_ON_L				= 0xFA
-	__ALLLED_ON_H				= 0xFB
-	__ALLLED_OFF_L			 = 0xFC
-	__ALLLED_OFF_H			 = 0xFD
+	__SUBADR1				= 0x02
+	__SUBADR2				= 0x03
+	__SUBADR3				= 0x04
+	__MODE1					= 0x00
+	__MODE2					= 0x01
+	__PRESCALE				= 0xFE
+	__LED0_ON_L				= 0x06
+	__LED0_ON_H				= 0x07
+	__LED0_OFF_L			= 0x08
+	__LED0_OFF_H			= 0x09
+	__ALLLED_ON_L			= 0xFA
+	__ALLLED_ON_H			= 0xFB
+	__ALLLED_OFF_L			= 0xFC
+	__ALLLED_OFF_H			= 0xFD
 
 	def __init__(self, address=0x40, debug=False):
 		self.i2c = Adafruit_I2C(address)
