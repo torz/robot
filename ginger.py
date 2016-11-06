efac2218f44326d0772e4a65b882258415a6473c
torz <torz@gmx.com>
2016-11-06 12:47:55 +0000

    fixed directions

--- ginger.py
+++ ginger.py
@@ -17,10 +17,11 @@ class Ginger():
     def forward(self):
         print('forward')
         self.setup()
-        GPIO.output(17, True)
-        GPIO.output(18, False)
-        GPIO.output(26, True)
-        GPIO.output(20, False)
+        GPIO.output(17, False)
+        GPIO.output(18, True)
+        GPIO.output(26, False)
+        GPIO.output(20, True)
+
         sleep(self.runtime)
         GPIO.cleanup()
 
@@ -28,30 +29,33 @@ class Ginger():
     def reverse(self):
         print('reverse')
         self.setup()
-        GPIO.output(17, False)
-        GPIO.output(18, True)
-        GPIO.output(26, False)
-        GPIO.output(20, True)
+        GPIO.output(17, True)
+        GPIO.output(18, False)
+        GPIO.output(26, True)
+        GPIO.output(20, False)
+
         sleep(self.runtime)
         GPIO.cleanup()
 
     def left(self):
         print('left')
         self.setup()
-        GPIO.output(17, True)
-        GPIO.output(18, False)
-        GPIO.output(26, False)
-        GPIO.output(20, True)
+
+        GPIO.output(17, False)
+        GPIO.output(18, True)
+        GPIO.output(26, True)
+        GPIO.output(20, False)
         sleep(self.runtime)
         GPIO.cleanup()
 
     def right(self):
         print('right')
         self.setup()
-        GPIO.output(17, False)
-        GPIO.output(18, True)
-        GPIO.output(26, True)
-        GPIO.output(20, False)
+
+        GPIO.output(17, True)
+        GPIO.output(18, False)
+        GPIO.output(26, False)
+        GPIO.output(20, True)
         sleep(self.runtime)
         GPIO.cleanup()
 
